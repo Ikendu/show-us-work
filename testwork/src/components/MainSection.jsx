cec53eab04ee20627e20725b052d3f52d5bf5ef1
Aniede Chibundu <ndubest56@gmail.com>
2024-08-02 11:30:17 +0100

    test project

--- testwork/src/components/MainSection.jsx
+++ testwork/src/components/MainSection.jsx
@@ -5,13 +5,13 @@ export default function MainSection() {
   return (
     <div className="p-10 pl-20 pr-20">
       <div className="flex justify-between">
-        <div className="flex gap-3 items-center bg-[#E3E2E2] rounded-md p-3 px-6">
+        <div className="flex gap-3 items-center bg-[#E3E2E2] rounded-md p-3 px-8">
           <p>Categories</p>
           <DownIcon />
         </div>
-        <div className="flex gap-3 items-center">
+        <div className="flex gap-3 items-center bg-[#E3E2E2] rounded-md px-8">
           <SearchIcon />
-          <p>Search</p>
+          <input placeholder="Search" className="bg-[#E3E2E2]" />
         </div>
       </div>
       <div></div>
